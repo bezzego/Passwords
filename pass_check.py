d976eea882a4ff1c1a516285cee9f1760a900b3c
Egor Bezborodov <egor.bezborodoff@gmail.com>
2025-03-18 11:15:03 +0300

    Initial commit

---
+++ pass_check.py
@@ -0,0 +1,32 @@
+password = input('Введите пароль:')
+
+def has_digit(pwd):
+    return any(char.isdigit() for char in pwd)
+
+
+def has_upper_letters(pwd):
+    return any(char.isupper() for char in pwd)
+
+
+def has_lower_letters(pwd):
+    return any(char.islower() for char in pwd)
+
+
+def is_very_long(pwd):
+    return len(pwd) >= 12
+
+
+def has_symbols(pwd):
+    return any(not char.isalnum() for char in pwd)
+
+
+checks = [has_digit, has_upper_letters, has_lower_letters, is_very_long, has_symbols]
+
+
+def balls_add_score(pwd):
+    return sum(2 for check in checks if check(pwd))
+
+
+score = balls_add_score(password)
+
+print(f'Рейтинг пароля: {score}')
